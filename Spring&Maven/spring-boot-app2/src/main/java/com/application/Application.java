39c5d512e43b6a0d035bc4f522ba92c0a6bc644a
BohdanSmetanyuk <bsmetanyuk@gmail.com>
2020-02-07 13:24:35 +0200

    Some refractoring for spring-boot-app2

--- Spring&Maven/spring-boot-app2/src/main/java/com/application/Application.java
+++ Spring&Maven/spring-boot-app2/src/main/java/com/application/Application.java
@@ -1,9 +1,15 @@
 package com.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.controller")
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
